1d6c34a80ba8140076ae7fb9c7fd676309594c6c
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-08-13 01:17:55 +0300

    bump version

--- studio/Python/setup.py
+++ studio/Python/setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="tinymovr",
-    version="0.2.0",
+    version="0.2.1",
     author="Yannis Chatzikonstantinou",
     author_email="info@tinymovr.com",
     description="Tinymovr Studio",
