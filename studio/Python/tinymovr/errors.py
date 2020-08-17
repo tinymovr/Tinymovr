7da5eb921f1af3556f0b1a3581f3fb73e36b7910
Ioannis Chatzikonstantinou <yconst@gmail.com>
2020-08-17 15:58:41 +0300

    update error list

--- studio/Python/tinymovr/errors.py
+++ studio/Python/tinymovr/errors.py
@@ -7,17 +7,21 @@ class ErrorIDs(Enum):
     IllegalValue = 2
     VBusUndervoltage = 3
     OverCurrent = 4
-    PhaseResistanceOutOfRange = 5
-    PhaseInductanceOutOfRange = 6
-    InvalidPolePairs = 7
+    PWMLimitExceeded = 5
+    PhaseResistanceOutOfRange = 6
+    PhaseInductanceOutOfRange = 7
+    InvalidPolePairs = 8
+    MagneticFieldOutOfRange = 9
 
 error_descriptions = {
     ErrorIDs.NoError: "No error",
     ErrorIDs.InvalidState: "Attempt to transition to invalid state",
     ErrorIDs.IllegalValue: "Illegal value passed to endpoint",
     ErrorIDs.VBusUndervoltage: "Power supply bus under low voltage threshold",
-    ErrorIDs.OverCurrent: "Phase current exceeded threshold",
+    ErrorIDs.OverCurrent: "Phase current limit has been exceeded",
+    ErrorIDs.PWMLimitExceeded: "Phase voltage modulation limit has been exceeded",
     ErrorIDs.PhaseResistanceOutOfRange: "Measured phase resistance is out of range",
     ErrorIDs.PhaseInductanceOutOfRange: "Measured phase inductance is out of range",
-    ErrorIDs.InvalidPolePairs: "Pole pairs calculation did not converge to a valid value"
+    ErrorIDs.InvalidPolePairs: "Pole pairs calculation did not converge to a valid value",
+    ErrorIDs.MagneticFieldOutOfRange: "The magnetic field sensed by the encoder is out of range"
 }    
