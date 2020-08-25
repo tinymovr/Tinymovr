b224cc5676df4fdcd97ce2057d44310b25ef949e
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-08-25 12:52:32 +0300

    refactor can channel discovery to make it more robust

--- studio/Python/tinymovr/iface/iface.py
+++ studio/Python/tinymovr/iface/iface.py
@@ -9,20 +9,20 @@ class IFace:
         self.bus = bus
 
 ifaces_to_devices = {
-    "slcan": "cantact",
-    "robotell": "CP210"
+    "slcan": ("canable", "cantact"),
+    "robotell": ("CP210", )
 }
 
 def guess_channel(iface_hint):
     '''
     Tries to guess a channel based on an interface hint.
     '''
-    device = ifaces_to_devices[iface_hint].lower()
-    ports = ([
-        p.device
-        for p in serial.tools.list_ports.comports()
-        if device in p.description.lower()
-    ])
+    device_strings = [s.lower() for s in ifaces_to_devices[iface_hint]]
+    ports = []
+    for p in serial.tools.list_ports.comports():
+        desc_lower = p.description.lower()
+        if any([s in desc_lower for s in device_strings]):
+            ports.append(p.device)
     if not ports:
         raise IOError("Could not autodiscover CAN channel")
     if len(ports) > 1:
