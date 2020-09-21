634a9d9df071dd062d0c499eacb0778a6204a9bd
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-09-22 00:48:30 +0300

    add test bus

---
+++ studio/Python/tinymovr/bus/test.py
@@ -0,0 +1,13 @@
+import can
+
+class Test(can.BusABC):
+
+    def __init__(self, channel, can_filters=None, **kwargs):
+        super().__init__(channel, can_filters, **kwargs)
+        self.channel_info = "Test Channel"
+
+    def send(self, msg: can.Message):
+        print(msg)
+
+    def _recv_internal(self, timeout) -> can.Message:
+        return '', True
