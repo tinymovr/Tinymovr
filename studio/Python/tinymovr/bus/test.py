9f70b6c43dc6ef16a039483a1933d9a6e2250379
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2020-09-24 10:51:44 +0300

    very basic simulation

--- studio/Python/tinymovr/bus/test.py
+++ studio/Python/tinymovr/bus/test.py
@@ -18,14 +18,18 @@ class Test(can.BusABC):
         self.buffer: can.Message = None
         self.codec = CANBusCodec()
         self.Kv = 100
+        self.CPR = 8192
 
         self.last_call_time = datetime.now()
 
         self.ep_func_map = {
             0x03: self._get_state,
+            0x07: self._set_state,
             0x09: self._get_encoder_estimates,
             0x0A: self._get_setpoints,
             0x0C: self._set_pos_setpoint,
+            0x0D: self._set_vel_setpoint,
+            0x0E: self._set_cur_setpoint,
             0x1A: self._get_device_info
         }
 
@@ -63,12 +67,25 @@ class Test(can.BusABC):
 
     def _update_state(self):
         time_now = datetime.now()
-        # diff = time_now - self.last_call_time
+        diff = (time_now - self.last_call_time).total_seconds()
         self.last_call_time = time_now
 
-        self._state["position_estimate"] = self._state["position_setpoint"]
-        self._state["velocity_estimate"] = self._state["velocity_setpoint"]
-        self._state["current_estimate"] = self._state["current_setpoint"]
+        if self._state["state"] == 2:
+            self._state["current_estimate"] = self._state["current_setpoint"]
+            torque = self._state["current_estimate"] / self.Kv
+            self._state["velocity_estimate"] += torque * self.CPR * diff
+            self._state["position_estimate"] += 1/2 * torque * self.CPR * diff * diff
+            if self._state["mode"] == 1:
+                self._state["current_estimate"] = 0
+                self._state["velocity_estimate"] = self._state["velocity_setpoint"]
+                self._state["position_estimate"] += self._state["velocity_estimate"] * diff
+            if self._state["mode"] == 2:
+                self._state["current_estimate"] = 0
+                self._state["velocity_estimate"] = 0
+                self._state["position_estimate"] = self._state["position_setpoint"]
+        if self._state["state"] == 0:
+            self._state["current_estimate"] = 0
+            self._state["velocity_estimate"] = 0
 
     # ---- Endpoint methods -----------------------------------------
 
@@ -79,6 +96,12 @@ class Test(can.BusABC):
         gen_payload = self.codec.serialize(vals,
                                            *Endpoints["state"]["types"])
         self.buffer = create_frame(self.node_id, 0x03, False, gen_payload)
+
+    def _set_state(self, payload):
+        vals = self.codec.deserialize(payload,
+                                      *Endpoints["set_state"]["types"])
+        self._state["state"] = vals[0]
+        self._state["mode"] = vals[1]
 
     def _get_device_info(self, payload):
         vals = (0, 0, 7, 1, 25)
@@ -106,3 +129,14 @@ class Test(can.BusABC):
         self._state["position_setpoint"] = vals[0]
         self._state["velocity_setpoint"] = vals[1]
         self._state["current_setpoint"] = vals[2]
+
+    def _set_vel_setpoint(self, payload):
+        vals = self.codec.deserialize(payload,
+                                      *Endpoints["set_vel_setpoint"]["types"])
+        self._state["velocity_setpoint"] = vals[0]
+        self._state["current_setpoint"] = vals[1]
+
+    def _set_cur_setpoint(self, payload):
+        vals = self.codec.deserialize(payload,
+                                      *Endpoints["set_cur_setpoint"]["types"])
+        self._state["current_setpoint"] = vals[0]
