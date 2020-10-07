97f8559b23b4f15f476a643310290260a9e8c7aa
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-10-08 00:41:02 +0300

    make endpoints use units

--- studio/Python/tinymovr/__init__.py
+++ studio/Python/tinymovr/__init__.py
@@ -2,4 +2,4 @@ from tinymovr.errors import ErrorIDs, error_descriptions
 from tinymovr.tinymovr import Tinymovr
 from tinymovr.user_wrapper import UserWrapper
 from tinymovr.shell import spawn_shell
-import units
+import tinymovr.units
--- studio/Python/tinymovr/units.py
+++ studio/Python/tinymovr/units.py
@@ -6,6 +6,6 @@ _registry = None
 def get_registry():
     global _registry
     if not _registry:
-        _registry = UnitRegistry()
+        _registry = pint.UnitRegistry()
         _registry.define('tick = turn / 8192')
     return _registry
