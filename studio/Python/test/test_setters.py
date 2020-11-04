89524cc70aeb4d54b65dd5ef77f5850ebb5728d7
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-11-04 23:18:35 +0200

    use units in tests

--- studio/Python/test/test_setters.py
+++ studio/Python/test/test_setters.py
@@ -6,8 +6,11 @@ import tinymovr
 from tinymovr import Tinymovr, ErrorIDs
 from tinymovr.iface import IFace
 from tinymovr.iface.can import CAN
+from tinymovr.units import get_registry
 
 import unittest
+
+ureg = get_registry()
 
 class TestSetters(unittest.TestCase):
 
@@ -25,7 +28,7 @@ class TestSetters(unittest.TestCase):
         time.sleep(0.5)
         self.tm.set_cur_setpoint(-0.5)
         time.sleep(0.5)
-        self.assertLess(abs(self.tm.encoder_estimates.velocity), 500)
+        self.assertLess(abs(self.tm.encoder_estimates.velocity), 500 * ureg.ticks)
 
     def test_set_vel_control(self):
         self.tm.current_control()
@@ -33,7 +36,7 @@ class TestSetters(unittest.TestCase):
         time.sleep(0.5)
         self.tm.set_vel_setpoint(-1000)
         time.sleep(0.5)
-        self.assertLess(abs(self.tm.encoder_estimates.position), 500)
+        self.assertLess(abs(self.tm.encoder_estimates.position), 500 * ureg.ticks)
 
 
 
