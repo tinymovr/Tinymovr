77247fb4bbc1b44ae0d664f6f3ddec494dbb5ee6
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-11-21 17:00:46 +0200

    increase i trip threshold

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -43,7 +43,7 @@
 // Limits
 #define PWM_LIMIT                   (0.8f)
 #define I_INTEGRATOR_DECAY_FACTOR   (0.995f)
-#define I_TRIP_MARGIN               (1.25f)
+#define I_TRIP_MARGIN               (1.35f)
 #define VBUS_LOW_THRESHOLD          (11.0f)
 
 // Calibration
