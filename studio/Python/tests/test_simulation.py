4779ee733b7d64708ddb80746c8eb27b0db73b2c
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-11-29 01:35:58 +0200

    update tests according to new simulation additions

--- studio/Python/tests/test_simulation.py
+++ studio/Python/tests/test_simulation.py
@@ -30,12 +30,18 @@ def get_tm() -> Tinymovr:
 
 class TestSimulation(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.tm: Tinymovr = get_tm()
+
+    def setUp(self):
+        self.tm.reset()
+
     def test_get_device_info(self):
         '''
         Test getting of device info
         '''
-        tm: Tinymovr = get_tm()
-        info = tm.device_info
+        info = self.tm.device_info
         self.assertGreaterEqual(info.fw_major, 0)
         self.assertGreaterEqual(info.fw_minor, 7)
 
@@ -44,8 +50,7 @@ class TestSimulation(unittest.TestCase):
         Test successful getting of correct error codes
         in various scenarios
         '''
-        tm: Tinymovr = get_tm()
-        state = tm.state
+        state = self.tm.state
         error_id = ErrorIDs(state.error)
         self.assertEqual(error_id, ErrorIDs.NoError)
         
@@ -54,9 +59,8 @@ class TestSimulation(unittest.TestCase):
         Test successful getting of correct error codes
         in various scenarios
         '''        
-        tm: Tinymovr = get_tm()
-        tm.position_control()
-        state = tm.state
+        self.tm.position_control()
+        state = self.tm.state
         error_id = ErrorIDs(state.error)
         self.assertEqual(error_id, ErrorIDs.InvalidState)
 
@@ -65,62 +69,56 @@ class TestSimulation(unittest.TestCase):
         Test successful getting of correct error codes
         in various scenarios
         '''        
-        tm: Tinymovr = get_tm()
-        tm.calibrate()
-        tm.position_control()
-        state = tm.state
+        self.tm.calibrate()
+        self.tm.position_control()
+        state = self.tm.state
         error_id = ErrorIDs(state.error)
         self.assertEqual(error_id, ErrorIDs.NoError)
 
     def test_get_encoder_estimates(self):
-        tm: Tinymovr = get_tm()
-        estimates = tm.encoder_estimates
+        estimates = self.tm.encoder_estimates
         self.assertEqual(estimates.position, 0)
         self.assertEqual(estimates.velocity, 0)
 
     def test_set_current_control(self):
-        tm: Tinymovr = get_tm()
-        tm.calibrate()
-        tm.current_control()
-        tm.set_cur_setpoint(0.5 * A)
-        self.assertEqual(tm.Iq.estimate, 0.5 * A)
+        self.tm.calibrate()
+        self.tm.current_control()
+        self.tm.set_cur_setpoint(0.5 * A)
+        self.assertEqual(self.tm.Iq.estimate, 0.5 * A)
         time.sleep(0.5)
-        tm.set_cur_setpoint(-0.5 * A)
-        self.assertEqual(tm.Iq.estimate, -0.5 * A)
+        self.tm.set_cur_setpoint(-0.5 * A)
+        self.assertEqual(self.tm.Iq.estimate, -0.5 * A)
         time.sleep(0.5)
-        self.assertLess(abs(tm.encoder_estimates.velocity), 500 * ticks/s)
+        self.assertLess(abs(self.tm.encoder_estimates.velocity), 500 * ticks/s)
 
     def test_set_current_control_nounits(self):
-        tm: Tinymovr = get_tm()
-        tm.calibrate()
-        tm.current_control()
-        tm.set_cur_setpoint(0.5)
-        self.assertEqual(tm.Iq.estimate.magnitude, 0.5)
+        self.tm.calibrate()
+        self.tm.current_control()
+        self.tm.set_cur_setpoint(0.5)
+        self.assertEqual(self.tm.Iq.estimate.magnitude, 0.5)
         time.sleep(0.5)
-        tm.set_cur_setpoint(-0.5)
-        self.assertEqual(tm.Iq.estimate.magnitude, -0.5)
+        self.tm.set_cur_setpoint(-0.5)
+        self.assertEqual(self.tm.Iq.estimate.magnitude, -0.5)
         time.sleep(0.5)
-        self.assertLess(abs(tm.encoder_estimates.velocity.magnitude), 500)
+        self.assertLess(abs(self.tm.encoder_estimates.velocity.magnitude), 500)
 
     def test_set_vel_control(self):
-        tm: Tinymovr = get_tm()
-        tm.calibrate()
-        tm.current_control()
-        tm.set_vel_setpoint(1000 * ticks/s)
+        self.tm.calibrate()
+        self.tm.current_control()
+        self.tm.set_vel_setpoint(1000 * ticks/s)
         time.sleep(0.5)
-        tm.set_vel_setpoint(-1000 * ticks/s)
+        self.tm.set_vel_setpoint(-1000 * ticks/s)
         time.sleep(0.5)
-        self.assertLess(abs(tm.encoder_estimates.position), 500 * ticks)
+        self.assertLess(abs(self.tm.encoder_estimates.position), 500 * ticks)
 
     def test_set_vel_control_nounits(self):
-        tm: Tinymovr = get_tm()
-        tm.calibrate()
-        tm.current_control()
-        tm.set_vel_setpoint(1000)
+        self.tm.calibrate()
+        self.tm.current_control()
+        self.tm.set_vel_setpoint(1000)
         time.sleep(0.5)
-        tm.set_vel_setpoint(-1000)
+        self.tm.set_vel_setpoint(-1000)
         time.sleep(0.5)
-        self.assertLess(abs(tm.encoder_estimates.position.magnitude), 500)
+        self.assertLess(abs(self.tm.encoder_estimates.position.magnitude), 500)
 
 
 if __name__ == '__main__':
