89c3174a6e534876dd3821c927d1cf93a72b9017
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-12-28 16:30:57 +0200

    add test for rejection of invalid limits

--- studio/Python/tests/test_board.py
+++ studio/Python/tests/test_board.py
@@ -36,14 +36,22 @@ class TestBoard(TMTestCase):
         # apparently the statistics lib works with quantities only
         self.assertLess(st.pstdev(pos_estimates) * ticks, 5 * ticks)
 
-    def test_b_calibrate(self):
+    def test_b_invalid_limits(self):
+        '''
+        Test rejection of invalid limits
+        '''
+        limits = self.tm.limits
+        self.tm.set_limits(-10, -10) # invalid, should not be set
+        self.assertEqual(limits, self.tm.limits)
+
+    def test_c_calibrate(self):
         '''
         Test board calibration if not calibrated
         '''
         self.check_state(0)
         self.try_calibrate()
 
-    def test_c_position_control(self):
+    def test_d_position_control(self):
         '''
         Test position control
         '''
@@ -58,7 +66,7 @@ class TestBoard(TMTestCase):
             self.assertAlmostEqual(i*1000*ticks, self.tm.encoder_estimates.position, delta=1000*ticks)
             time.sleep(0.4)
 
-    def test_d_velocity_control(self):
+    def test_e_velocity_control(self):
         '''
         Test velocity control
         '''
@@ -102,7 +110,7 @@ class TestBoard(TMTestCase):
         for target, estimate in velocity_pairs:
             self.assertAlmostEqual(target, estimate, delta=30000*ticks/s)
 
-    def test_e_random_pos_control(self):
+    def test_f_random_pos_control(self):
         '''
         Test random positions
         '''
@@ -116,7 +124,7 @@ class TestBoard(TMTestCase):
             self.tm.set_pos_setpoint(new_pos * ticks)
             time.sleep(0.5)
 
-    def test_f_limits(self):
+    def test_g_limits(self):
         '''
         Test limits
         '''
