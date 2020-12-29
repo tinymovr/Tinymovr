37bc1ca99cb88458cc14a35e793840dad494798a
Yannis Chatzikonstantinou <yconst@gmail.com>
2020-12-29 18:22:09 +0200

    add integrator gain to UART protocol

--- firmware/src/uart/uart_interface.c
+++ firmware/src/uart/uart_interface.c
@@ -53,16 +53,20 @@ void UART_WriteAddr(uint8_t addr, int32_t data)
 			Controller_SetIqSetpoint(data * ONE_OVER_UART_I_SCALING_FACTOR);
 		break;
 
+		case 'G': // velocity integrator gain
+			Controller_SetVelIntegratorGain(data * ONE_OVER_UART_VEL_INT_SCALING_FACTOR);
+		break;
+
 		case 'U': // CAN Baud Rate
-		CAN_SetkBaudRate(data);
+			CAN_SetkBaudRate(data);
 		break;
 
 		case 'C': // CAN ID
-        CAN_SetID(data);
+			CAN_SetID(data);
         break;
 
 		default:
-		// No action
+			// No action
 		break;
 	}
 }
@@ -73,88 +77,92 @@ int32_t UART_ReadAddr(uint8_t addr)
 	switch (addr)
 	{
 		case 'b': // vbus value
-		ret_val = (int32_t)(ADC_GetVBus() * UART_V_SCALING_FACTOR);
+			ret_val = (int32_t)(ADC_GetVBus() * UART_V_SCALING_FACTOR);
 		break;
 
 		case 'e': // controller error
-		{uint8_t *error_flags = get_error_flags();
-		memcpy(&ret_val, error_flags, sizeof(uint32_t));}
+			{uint8_t *error_flags = get_error_flags();
+			memcpy(&ret_val, error_flags, sizeof(uint32_t));}
 		break;
 
 		case 'o': // encoder pos
-		ret_val =  MA_GetAngle();
+			ret_val =  MA_GetAngle();
 		break;
 
 		case 'p': // pos estimate
-		ret_val = Observer_GetPosEstimate();
+			ret_val = Observer_GetPosEstimate();
 		break;
 
 		case 'P': // pos setpoint
-		ret_val = Controller_GetPosSetpoint();
+			ret_val = Controller_GetPosSetpoint();
 		break;
 
 		case 'v': // vel estimate
-		ret_val = (int32_t)Observer_GetVelEstimate();
+			ret_val = (int32_t)Observer_GetVelEstimate();
 		break;
 
 		case 'V': // vel setpoint
-		ret_val = (int32_t)Controller_GetVelSetpoint();
+			ret_val = (int32_t)Controller_GetVelSetpoint();
 		break;
 
 		case 'i': // current estimate
-		ret_val = (int32_t)(Controller_GetIqEstimate() * UART_I_SCALING_FACTOR);
+			ret_val = (int32_t)(Controller_GetIqEstimate() * UART_I_SCALING_FACTOR);
 		break;
 
 		case 'I': // current setpoint
-		ret_val = (int32_t)(Controller_GetIqSetpoint() * UART_I_SCALING_FACTOR);
+			ret_val = (int32_t)(Controller_GetIqSetpoint() * UART_I_SCALING_FACTOR);
+		break;
+
+		case 'G': // velocity integrator setpoint
+			ret_val = (int32_t)(Controller_GetVelIntegratorGain() * UART_VEL_INT_SCALING_FACTOR);
 		break;
 
 		case 'd': // observer direction
-		ret_val = Observer_GetDirection();
+			ret_val = Observer_GetDirection();
 		break;
 
 		case 'h': // phase resistance
-		ret_val = Motor_GetPhaseResistance() * UART_R_SCALING_FACTOR;
+			ret_val = Motor_GetPhaseResistance() * UART_R_SCALING_FACTOR;
 		break;
 
 		case 'l': // phase inductance
-		ret_val = Motor_GetPhaseInductance() * UART_L_SCALING_FACTOR;
+			ret_val = Motor_GetPhaseInductance() * UART_L_SCALING_FACTOR;
 		break;
 
 		case 'U': // CAN Baud Rate
-		ret_val = CAN_GetkBaudRate();
+			ret_val = CAN_GetkBaudRate();
 		break;
 
 		case 'C': // CAN ID
-        ret_val = CAN_GetID();
+			ret_val = CAN_GetID();
         break;
 
 		case 'Q': // calibrate
-		Controller_SetState(STATE_CALIBRATE);
+			Controller_SetState(STATE_CALIBRATE);
 		break;
 
 		case 'A': // closed loop
-		Controller_SetState(STATE_CL_CONTROL);
+			Controller_SetState(STATE_CL_CONTROL);
 		break;
 
 		case 'Z': // idle
-		Controller_SetState(STATE_IDLE);
+			Controller_SetState(STATE_IDLE);
 		break;
 
 		case 'R': // reset mcu
-		system_reset();
+			system_reset();
 		break;
 
 		case 'S': // save config
-		NVM_SaveConfig();
+			NVM_SaveConfig();
 		break;
 
 		case 'X': // erase config
-		NVM_Erase();
+			NVM_Erase();
 		break;
 
 		default:
-		// No action
+			// No action
 		break;
 
 	}
