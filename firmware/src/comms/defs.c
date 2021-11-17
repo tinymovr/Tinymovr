9c79a657a1aede12300440d87118c32ead30fd14
Yannis Chatzikonstantinou <yconst@gmail.com>
2021-11-18 01:16:54 +0200

    also pass types

--- firmware/src/comms/defs.c
+++ firmware/src/comms/defs.c
@@ -28,17 +28,30 @@ size_t vel_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out)
 size_t save_config_caller(uint8_t *buffer_in, uint8_t *buffer_out) {NVM_SaveConfig();return 0;}
 size_t reset_caller(uint8_t *buffer_in, uint8_t *buffer_out) {system_reset();return 0;}
 
+    // VOID = 0
+    // BOOL = 1
+    // UINT8 = 2
+    // INT8 = 3
+    // UINT16 = 4
+    // INT16 = 5
+    // UINT32 = 6
+    // INT32 = 7
+    // UINT64 = 8
+    // INT64 = 9
+    // FLOAT = 10
+    // DOUBLE = 11
+
 RemoteObject *make_system(void)
 {
-    MAKE_ATTR(pos_est, pos_est_user_frame_getter, noop)
-    MAKE_ATTR(vel_est, vel_est_user_frame_getter, noop)
+    MAKE_ATTR(pos_est, 10, pos_est_user_frame_getter, noop)
+    MAKE_ATTR(vel_est, 10, vel_est_user_frame_getter, noop)
     //MAKE_ATTR(D, pos_est_getter, pos_est_setter)
     MAKE_OBJECT(encoder, &pos_est, &vel_est)
 
     //MAKE_ATTR(counter, counter_getter, counter_setter)
     //MAKE_ATTR(version, version_getter, version_setter)
-    MAKE_FUNC(save, save_config_caller)
-    MAKE_FUNC(reset, reset_caller)
+    MAKE_FUNC(save, 0, save_config_caller)
+    MAKE_FUNC(reset, 0, reset_caller)
     MAKE_OBJECT(system, &encoder, &save, &reset)
     return &system;
 }
