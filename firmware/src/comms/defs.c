087f9dfd1cee566e9065ef8dd366d3c4df93e2b4
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2021-11-19 12:52:15 +0200

    add more endpoints

--- firmware/src/comms/defs.c
+++ firmware/src/comms/defs.c
@@ -1,6 +1,7 @@
 
 #include <avlos/avlos.h>
 #include <src/observer/observer.h>
+#include <src/controller/controller.h>
 #include <src/nvm/nvm.h>
 #include <src/scheduler/scheduler.h>
 #include <src/system/system.h>
@@ -12,22 +13,52 @@ Server s;
 
 size_t noop(uint8_t *buffer_in, uint8_t *buffer_out) {return 0;}
 
-size_t pos_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out)
-{
+size_t pos_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
     size_t v = 0;
     write_le(observer_get_pos_estimate_user_frame(), buffer_out, &v);
     return v;
 }
 
-size_t vel_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out)
-{
+size_t vel_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
     size_t v = 0;
     write_le(observer_get_vel_estimate_user_frame(), buffer_out, &v);
     return v;
 }
 
-size_t busy_cycles_getter(uint8_t *buffer_in, uint8_t *buffer_out)
-{
+size_t Iq_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    size_t v = 0;
+    write_le(controller_get_Iq_estimate_user_frame(), buffer_out, &v);
+    return v;
+}
+
+size_t pos_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    size_t v = 0;
+    write_le(controller_get_pos_setpoint_user_frame(), buffer_out, &v);
+    return v;
+}
+size_t pos_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    
+}
+
+size_t vel_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    size_t v = 0;
+    write_le(controller_get_vel_setpoint_user_frame(), buffer_out, &v);
+    return v;
+}
+size_t vel_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    
+}
+
+size_t Iq_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    size_t v = 0;
+    write_le(controller_get_Iq_estimate_user_frame(), buffer_out, &v);
+    return v;
+}
+size_t Iq_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
+    
+}
+
+size_t busy_cycles_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
     size_t v = 0;
     write_le(Scheduler_GetBusyCycles(), buffer_out, &v);
     return v;
@@ -55,6 +86,12 @@ RemoteObject *make_system(void)
     MAKE_ATTR(vel_est, 10, vel_est_user_frame_getter, noop)
     //MAKE_ATTR(D, pos_est_getter, pos_est_setter)
     MAKE_OBJECT(encoder, &pos_est, &vel_est)
+
+    MAKE_ATTR(pos_set, 10, pos_set_user_frame_getter, pos_set_user_frame_setter)
+    MAKE_ATTR(vel_set, 10, vel_set_user_frame_getter, vel_set_user_frame_setter)
+    MAKE_ATTR(Iq_est, 10, Iq_est_user_frame_getter, noop)
+    MAKE_ATTR(Iq_set, 10, Iq_set_user_frame_getter, Iq_set_user_frame_setter)
+    MAKE_OBJECT(controller, &pos_set, &vel_set, &Iq_est, &Iq_set)
 
     //MAKE_ATTR(counter, counter_getter, counter_setter)
     //MAKE_ATTR(version, version_getter, version_setter)
--- firmware/src/comms/defs.h
+++ firmware/src/comms/defs.h
@@ -2,8 +2,22 @@
 #pragma once
 
 size_t noop(uint8_t *buffer_in, uint8_t *buffer_out);
+
 size_t pos_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+
 size_t vel_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+
+size_t Iq_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+
+size_t pos_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+size_t pos_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out);
+
+size_t vel_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+size_t vel_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out);
+
+size_t Iq_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out);
+size_t Iq_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out);
+
 size_t save_config_caller(uint8_t *buffer_in, uint8_t *buffer_out);
 size_t reset_caller(uint8_t *buffer_in, uint8_t *buffer_out);
 
