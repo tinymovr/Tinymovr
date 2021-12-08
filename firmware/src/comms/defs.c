925ebb39d5dc4dee6564251e74c4b9c4c681321b
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2021-12-08 17:12:09 +0200

    compact defs

--- firmware/src/comms/defs.c
+++ firmware/src/comms/defs.c
@@ -8,91 +8,29 @@
 #include <src/comms/defs.h>
 
 RemoteObject *store[50];
-
 Server s;
 
 size_t noop(uint8_t *buffer_in, uint8_t *buffer_out) {return 0;}
 
 // --- Encoder
-
-size_t pos_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(observer_get_pos_estimate_user_frame(), buffer_out, &v);
-    return v;
-}
-
-size_t vel_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(observer_get_vel_estimate_user_frame(), buffer_out, &v);
-    return v;
-}
+MAKE_GETTER(observer_get_pos_estimate_user_frame)
+MAKE_GETTER(observer_get_vel_estimate_user_frame)
 
 // --- Controller
-
-size_t Iq_est_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(controller_get_Iq_estimate_user_frame(), buffer_out, &v);
-    return v;
-}
-
-size_t pos_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(controller_get_pos_setpoint_user_frame(), buffer_out, &v);
-    return v;
-}
-size_t pos_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    float v = 0;
-    read_le(&v, buffer_in);
-    controller_set_pos_setpoint_user_frame(v);
-    return 0;
-}
-
-size_t vel_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(controller_get_vel_setpoint_user_frame(), buffer_out, &v);
-    return v;
-}
-size_t vel_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    float v = 0;
-    read_le(&v, buffer_in);
-    controller_set_vel_setpoint_user_frame(v);
-    return 0;
-}
-
-size_t Iq_set_user_frame_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(controller_get_Iq_estimate_user_frame(), buffer_out, &v);
-    return v;
-}
-size_t Iq_set_user_frame_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    float v = 0;
-    read_le(&v, buffer_in);
-    controller_set_Iq_setpoint_user_frame(v);
-    return 0;
-}
+MAKE_GETTER(controller_get_Iq_estimate_user_frame)
+MAKE_GETTER(controller_get_pos_setpoint_user_frame)
+MAKE_SETTER(controller_set_pos_setpoint_user_frame)
+MAKE_GETTER(controller_get_vel_setpoint_user_frame)
+MAKE_SETTER(controller_set_vel_setpoint_user_frame)
+MAKE_GETTER(controller_get_Iq_setpoint_user_frame)
+MAKE_SETTER(controller_set_Iq_setpoint_user_frame)
 
 // --- CAN
-
-size_t can_id_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(CAN_get_ID(), buffer_out, &v);
-    return v;
-}
-
-size_t can_id_setter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    uint8_t v = 0;
-    read_le(&v, buffer_in);
-    CAN_set_ID(v);
-    return 0;
-}
+MAKE_GETTER(CAN_get_ID)
+MAKE_SETTER(CAN_set_ID)
 
 // --- System
-
-size_t busy_cycles_getter(uint8_t *buffer_in, uint8_t *buffer_out) {
-    size_t v = 0;
-    write_le(Scheduler_GetBusyCycles(), buffer_out, &v);
-    return v;
-}
+MAKE_GETTER(Scheduler_GetBusyCycles)
 
 size_t save_config_caller(uint8_t *buffer_in, uint8_t *buffer_out) {NVM_SaveConfig();return 0;}
 size_t reset_caller(uint8_t *buffer_in, uint8_t *buffer_out) {system_reset();return 0;}
@@ -112,25 +50,25 @@ size_t reset_caller(uint8_t *buffer_in, uint8_t *buffer_out) {system_reset();ret
 
 RemoteObject *make_system(void)
 {
-    MAKE_ATTR(pos_est, 10, pos_est_user_frame_getter, noop)
-    MAKE_ATTR(vel_est, 10, vel_est_user_frame_getter, noop)
+    MAKE_ATTR(pos_est, 10, observer_get_pos_estimate_user_frame_getter, noop)
+    MAKE_ATTR(vel_est, 10, observer_get_vel_estimate_user_frame_getter, noop)
     //MAKE_ATTR(D, pos_est_getter, pos_est_setter)
     MAKE_OBJECT(encoder, &pos_est, &vel_est)
 
-    MAKE_ATTR(pos_set, 10, pos_set_user_frame_getter, pos_set_user_frame_setter)
-    MAKE_ATTR(vel_set, 10, vel_set_user_frame_getter, vel_set_user_frame_setter)
-    MAKE_ATTR(Iq_est, 10, Iq_est_user_frame_getter, noop)
-    MAKE_ATTR(Iq_set, 10, Iq_set_user_frame_getter, Iq_set_user_frame_setter)
+    MAKE_ATTR(pos_set, 10, controller_get_pos_setpoint_user_frame_getter, controller_set_pos_setpoint_user_frame_setter)
+    MAKE_ATTR(vel_set, 10, controller_get_vel_setpoint_user_frame_getter, controller_set_vel_setpoint_user_frame_setter)
+    MAKE_ATTR(Iq_est, 10, controller_get_Iq_estimate_user_frame_getter, noop)
+    MAKE_ATTR(Iq_set, 10, controller_get_Iq_setpoint_user_frame_getter, controller_set_Iq_setpoint_user_frame_setter)
     MAKE_OBJECT(controller, &pos_set, &vel_set, &Iq_est, &Iq_set)
 
-    MAKE_ATTR(can_id, 2, can_id_getter, can_id_setter)
+    MAKE_ATTR(can_id, 2, CAN_get_ID_getter, CAN_set_ID_setter)
     MAKE_OBJECT(can, &can_id)
 
     //MAKE_ATTR(counter, counter_getter, counter_setter)
     //MAKE_ATTR(version, version_getter, version_setter)
     MAKE_FUNC(save, 0, save_config_caller)
     MAKE_FUNC(reset, 0, reset_caller)
-    MAKE_ATTR(busy_cycles, 6, busy_cycles_getter, noop)
+    MAKE_ATTR(busy_cycles, 6, Scheduler_GetBusyCycles_getter, noop)
     MAKE_OBJECT(system, &encoder, &controller, &can, &busy_cycles, &save, &reset)
     return &system;
 }
