ac5eb35e860cde348e1345c1a16287bbde52038a
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-01-04 12:50:02 +0200

    progress

--- firmware/src/encoder/hfi.c
+++ firmware/src/encoder/hfi.c
@@ -1,31 +1,45 @@
 
 
+#include <src/adc/adc.h>
 #include <src/encoder/hfi.h>
+
+static HFIState state = {0};
 
 void hfi_update(void)
 {
-    if ((state.current_step & 0x2u) == 0x2u) // even
+	uint8_t next_sign = 1;
+	const uint8_t lut_index = state.current_step * HFI_SAMPLE_STEP;
+	const float I_meas_a = state.I_phase_meas.A;
+    const float I_meas_b = one_by_sqrt3 * (state.I_phase_meas.B - state.I_phase_meas.C);
+
+    if (1 == state.substep) // even
     { 
-        // get current measurement, Ihfi = Imeas - Iprev
-        // store Ihfi for current angle
-        // calculate next hfi_voltage vector in ab frame, convert to uvw
+		state.I_diffs[state.current_step] = 
+			(I_meas_a * utils_tab_cos_32_1[lut_index] + I_meas_b * utils_tab_sin_32_1[lut_index]) - state.I_prev;
+		next_sign = -1;
+		state.substep = 0;
+		state.current_step++;
     }
     else // odd
     {
-        // get current measurement, Iprev = Imeas
-        // calculate next hfi_voltage vector in ab frame, convert to uvw
+		state.I_prev = I_meas_a * utils_tab_cos_32_1[lut_index] + I_meas_b * utils_tab_sin_32_1[lut_index];
+		state.substep = 1;		
     }
 
-    state.current_step++;
+	const float mod_val = HFI_VOLTAGE / ADC_GetVBus();
+	const float mod_a = next_sign * mod_val * utils_tab_cos_32_1[lut_index];
+	const float mod_b = next_sign * mod_val * utils_tab_sin_32_1[lut_index];
+	SVM(mod_a, mod_b, &state.modulation_values.A,
+		&state.modulation_values.B, &state.modulation_values.C);
 
     if (state.current_step == HFI_SAMPLE_COUNT) {
         float c_real, c_im;
-        fft_8_bin1(state.I_diff_buffer, c_real, c_im);
+        fft_8_bin1(state.I_diffs, c_real, c_im);
 
         const float f_mag = sqrtf(c_real * c_real + c_im * c_im);
         const float f_phase = atan2f(c_im, c_real);
 
-
+		// compute & store angle
 
         state.current_step = 0;
     }
--- firmware/src/encoder/hfi.h
+++ firmware/src/encoder/hfi.h
@@ -1,10 +1,14 @@
 
 #define HFI_SAMPLE_COUNT 8
+#define HFI_SAMPLE_STEP (32/HFI_SAMPLE_COUNT)
+#define HFI_VOLTAGE 1.0
 
 typedef struct {
     float I_diffs[HFI_SAMPLE_COUNT];
     uint8_t current_step;
+    uint8_t substep;
     float angle_comp;
+    float I_prev;
 } HFIState;
 
 void hfi_update(void);
