31c8c4099a7833521d2d2558d017fe0fc74e9dff
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-01-06 17:28:10 +0200

    store phase and magnitude

--- firmware/src/encoder/hfi.c
+++ firmware/src/encoder/hfi.c
@@ -1,5 +1,5 @@
 
-
+#include <math.h>
 #include <src/adc/adc.h>
 #include <src/controller/controller.h>
 #include <src/utils/utils.h>
@@ -11,7 +11,7 @@ static HFIState state = {0};
 void hfi_update(void)
 {
 	uint8_t next_sign = 1;
-	const uint8_t lut_index = state.current_step * HFI_SAMPLE_STEP;
+	const uint8_t lut_index = state.current_step * HFI_SAMPLE_STRIDE;
 	FloatTriplet I_phase_meas = {0.0f};
 	ADC_get_I_phase(&I_phase_meas);
 	const float I_meas_a = I_phase_meas.A;
@@ -20,20 +20,20 @@ void hfi_update(void)
     if (1 == state.substep) // even
     { 
 		state.I_diffs[state.current_step] = 
-			(I_meas_a * utils_tab_cos_32_1[lut_index] + I_meas_b * utils_tab_sin_32_1[lut_index]) - state.I_prev;
+			(I_meas_a * utils_tab_cos_32_2[lut_index] + I_meas_b * utils_tab_sin_32_2[lut_index]) - state.I_prev;
 		next_sign = -1;
 		state.substep = 0;
 		state.current_step++;
     }
     else // odd
     {
-		state.I_prev = I_meas_a * utils_tab_cos_32_1[lut_index] + I_meas_b * utils_tab_sin_32_1[lut_index];
+		state.I_prev = I_meas_a * utils_tab_cos_32_2[lut_index] + I_meas_b * utils_tab_sin_32_2[lut_index];
 		state.substep = 1;		
     }
 
 	const float mod_val = HFI_VOLTAGE / ADC_GetVBus();
-	const float mod_a = next_sign * mod_val * utils_tab_cos_32_1[lut_index];
-	const float mod_b = next_sign * mod_val * utils_tab_sin_32_1[lut_index];
+	const float mod_a = next_sign * mod_val * utils_tab_cos_32_2[lut_index];
+	const float mod_b = next_sign * mod_val * utils_tab_sin_32_2[lut_index];
 
 	FloatTriplet mod_values = {0.0f};
 	SVM(mod_a, mod_b, &mod_values.A, &mod_values.B, &mod_values.C);
@@ -43,9 +43,12 @@ void hfi_update(void)
         float c_real, c_im;
         utils_fft8_bin1(state.I_diffs, &c_real, &c_im);
 
-        const float f_mag = sqrtf(c_real * c_real + c_im * c_im);
+        const float f_mag = fast_sqrt(c_real * c_real + c_im * c_im);
         const float f_phase = atan2f(c_im, c_real);
 
+        state.magnitude = f_mag;
+        state.phase = f_phase;
+        
 		// compute & store angle
 
         state.current_step = 0;
--- firmware/src/encoder/hfi.h
+++ firmware/src/encoder/hfi.h
@@ -1,13 +1,15 @@
 
-#define HFI_SAMPLE_COUNT 8
-#define HFI_SAMPLE_STEP (32/HFI_SAMPLE_COUNT)
+#define HFI_SAMPLE_COUNT 16
+#define HFI_SAMPLE_STRIDE (32/HFI_SAMPLE_COUNT)
 #define HFI_VOLTAGE 1.0f
 
 typedef struct {
     float I_diffs[HFI_SAMPLE_COUNT];
     uint8_t current_step;
     uint8_t substep;
-    float angle_comp;
+    float angle;
+    float phase;
+    float magnitude;
     float I_prev;
 } HFIState;
 
