d9a3470ab6ff66aa57ba511ad79350519653b02c
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-01-22 01:33:57 +0200

    initial infra for node removal

--- studio/Python/tinymovr/discovery.py
+++ studio/Python/tinymovr/discovery.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 from avlos import get_object_tree
-from tinymovr.isotp_channel import ISOTPChannel
+from tinymovr.isotp_channel import ISOTPChannel, ResponseError
 from tinymovr.tee import Tee
 
 
@@ -15,14 +15,16 @@ class Discovery:
     Discover Tinymovr instances on the CAN bus using the periodically
     transmitted heartbeat frame.
     '''
-    def __init__(self, can_bus, appeared_cb, disappeared_cb, logger):
+    def __init__(self, can_bus, appeared_cb, disappeared_cb, logger, lost_timeout = 2.0):
         self.can_bus = can_bus
         self.logger = logger
         self.appeared_cb = appeared_cb
         self.disappeared_cb = disappeared_cb
+        self.lost_timeout = lost_timeout
 
         self.isotp_channels = {}
         self.nodes = {}
+        self.update_stamps = {}
 
         self.tee = Tee(can_bus, lambda msg: HEARTBEAT_BASE == msg.arbitration_id & HEARTBEAT_BASE)
 
@@ -37,14 +39,31 @@ class Discovery:
         correct tee, as such will not disrupt other receivers.
         '''
         while True:
+            now = time.time()
             msg = self.tee.recv()
             while msg:
                 node_id = msg.arbitration_id & 0x3F
+                if node_id in self.nodes:
+                    self.update_stamps[node_id] = now
                 if node_id not in self.nodes:
                     chan = ISOTPChannel(self.can_bus, node_id, self.logger)
                     self.isotp_channels[node_id] = chan
-                    node = get_object_tree(chan)
-                    self.nodes[node_id] = node
-                    self.appeared_cb(node, node_id)
+                    try:
+                        node = get_object_tree(chan)
+                        self.nodes[node_id] = node
+                        self.update_stamps[node_id] = now
+                        self.appeared_cb(node, node_id)
+                    except ResponseError as e:
+                        self.logger.error(e)
                 msg = self.tee.recv()
+            
+            # for_removal = set()
+            # for node_id, stamp in self.update_stamps.items():
+            #     if now - stamp > self.lost_timeout:
+            #         for_removal.add(node_id)
+            # for node_id in for_removal:
+            #     del self.nodes[node_id]
+            #     del self.isotp_channels[node_id]
+            #     del self.update_stamps[node_id]
+            #     self.disappeared_cb(node_id)
             time.sleep(0.5)
