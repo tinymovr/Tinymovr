2d07539fa84663af70f351fe324c0a212d6daeac
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-08-16 21:19:46 +0300

    add license header

--- firmware/src/watchdog/watchdog.h
+++ firmware/src/watchdog/watchdog.h
@@ -1,4 +1,22 @@
 
+//  * This file is part of the Tinymovr-Firmware distribution
+//  * (https://github.com/yconst/tinymovr-firmware).
+//  * 
+//  * Copyright (c) 2022 @eufrizz
+//  * Copyright (c) 2022 Ioannis Chatzikonstantinou.
+//  * 
+//  * This program is free software: you can redistribute it and/or modify  
+//  * it under the terms of the GNU General Public License as published by  
+//  * the Free Software Foundation, version 3.
+//  *
+//  * This program is distributed in the hope that it will be useful, but 
+//  * WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+//  * General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU General Public License 
+//  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 /*
 The watchdog is used as a timer for inactivity on the CAN bus.
 When it times out, it sets the control state back to idle as a failsafe if the ESC becomes disconnected.
