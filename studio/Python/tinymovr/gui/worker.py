ba0f5ab1c9af82e7a59ab5800bb479c856fb4ee9
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-09-13 12:48:31 +0300

    minor refactor and cleanup

--- studio/Python/tinymovr/gui/worker.py
+++ studio/Python/tinymovr/gui/worker.py
@@ -22,18 +22,19 @@ class Worker(QObject):
         self.logger = logger
 
         init_tee(can.Bus(bustype=bustype, channel=channel, bitrate=bitrate))
-        self.dsc = Discovery(
-            self.node_appeared, self.node_disappeared, self.logger
-        )
-        self.target_dt = 0.02
-        self.meas_dt = self.target_dt
-        self.rt_dt = 0.0
-        self.load = 0
+        self.dsc = Discovery(self.node_appeared, self.node_disappeared, self.logger)
+        
+        self.target_dt = 0.02 # target refresh period
+        self.tau = 0.01 # filter value for statistics
+        self.meas_dt = self.target_dt # measured (actual) refresh period
+        self.rt_dt = 0.0 # round-trip time
+        self.load = 0 # channel load, 0-1
+
         self.active_attrs = []
         self.dynamic_attrs = []
         self.tms_by_id = {}
         self.dynamic_attrs_last_update = {}
-        self.dynamic_attrs_update_period = 0.5 #sec
+        self.dynamic_attrs_update_period = 0.5  # sec
         self.running = True
 
     def run(self):
@@ -45,7 +46,9 @@ class Worker(QObject):
             QApplication.processEvents()
             busy_dt = time.time() - start_time
             if busy_dt < self.target_dt:
-                self.load = self.load * 0.99 + busy_dt/self.target_dt * 0.01
+                self.load = (
+                    self.load * (1 - self.tau) + busy_dt / self.target_dt * self.tau
+                )
                 time.sleep(self.target_dt - busy_dt)
                 self.meas_dt = self.target_dt
             else:
@@ -58,9 +61,7 @@ class Worker(QObject):
         self.running = False
 
     def get_attr_values(self):
-        """
-        
-        """
+        """ """
         # TODO: Handle possible exception
         vals = {}
         for attr in self.active_attrs:
@@ -81,7 +82,7 @@ class Worker(QObject):
         get_start_time = time.time()
         val = attr.get_value()
         get_dt = time.time() - get_start_time
-        self.rt_dt = self.rt_dt * 0.99 + get_dt * 0.01
+        self.rt_dt = self.rt_dt * (1 - self.tau) + get_dt * self.tau
         return val
 
     def node_appeared(self, node, name):
@@ -89,13 +90,13 @@ class Worker(QObject):
         self.tms_by_id[node_name] = node
         node.name = node_name
         node.include_base_name = True
-        self.dynamic_attrs = self.get_dynamic_attrs(self.tms_by_id)
+        self.dynamic_attrs = get_dynamic_attrs(self.tms_by_id)
         self.regen.emit(self.tms_by_id)
 
     def node_disappeared(self, name):
         node_name = "{}{}".format(base_node_name, name)
         del self.tms_by_id[node_name]
-        self.dynamic_attrs = self.get_dynamic_attrs(self.tms_by_id)
+        self.dynamic_attrs = get_dynamic_attrs(self.tms_by_id)
         self.regen.emit(self.tms_by_id)
 
     @QtCore.Slot(dict)
@@ -106,15 +107,16 @@ class Worker(QObject):
         elif d["enabled"] == False and attr in self.active_attrs:
             self.active_attrs.remove(attr)
 
-    def get_dynamic_attrs(self, attr_dict):
-        """
-        Get the attributes that are marked as dynamic in the spec.
-        """
-        dynamic_attrs = []
-        for _, attr in attr_dict.items():
-            if isinstance(attr, RemoteAttribute):
-                if attr.dynamic_value == True:
-                    dynamic_attrs.append(attr)
-            elif hasattr(attr, "remote_attributes"):
-                dynamic_attrs.extend(self.get_dynamic_attrs(attr.remote_attributes))
-        return dynamic_attrs
+
+def get_dynamic_attrs(attr_dict):
+    """
+    Get the attributes that are marked as dynamic in the spec.
+    """
+    dynamic_attrs = []
+    for _, attr in attr_dict.items():
+        if isinstance(attr, RemoteAttribute):
+            if attr.dynamic_value == True:
+                dynamic_attrs.append(attr)
+        elif hasattr(attr, "remote_attributes"):
+            dynamic_attrs.extend(get_dynamic_attrs(attr.remote_attributes))
+    return dynamic_attrs
