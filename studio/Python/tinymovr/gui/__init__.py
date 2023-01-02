4413cf23316200182e296cdb95c30b3de8da1ce6
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-01-02 16:53:01 +0200

    formatting with black

--- studio/Python/tinymovr/gui/__init__.py
+++ studio/Python/tinymovr/gui/__init__.py
@@ -1,4 +1,11 @@
-from tinymovr.gui.helpers import format_value, load_icon, app_stylesheet
+from tinymovr.gui.helpers import (
+    format_value,
+    load_icon,
+    app_stylesheet,
+    display_warning,
+    display_file_open_dialog,
+    display_file_save_dialog,
+)
 from tinymovr.gui.worker import Worker
 from tinymovr.gui.window import MainWindow
 from tinymovr.gui.gui import spawn
