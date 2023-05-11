6844ec0d866c4cefac1ef05ea47cdb41886e0ef6
Ioannis Chatzikonstantinou <yconst@gmail.com>
2023-05-11 14:41:45 +0300

    add homing unit test

---
+++ studio/Python/tests/test_homing.py
@@ -0,0 +1,38 @@
+"""
+This unit test suite tests the Homing functionality.
+"""
+import time
+import math
+
+from avlos.unit_field import get_registry
+
+import unittest
+from tests import TMTestCase
+
+ureg = get_registry()
+A = ureg.ampere
+ticks = ureg.ticks
+s = ureg.second
+tsleep = 0.18
+
+
+class TestHoming(TMTestCase):
+    def test_homing(self):
+        """
+        Test Homing
+        """
+        self.check_state(0)
+        self.try_calibrate()
+
+        self.tm.controller.position_mode()
+        self.check_state(2)
+
+        self.tm.homing.home()
+        time.sleep(self.tm.homing.max_homing_t)
+
+        self.assertEqual(self.tm.controller.mode, 0)
+        self.assertEqual(self.tm.homing.warnings, 0)
+
+
+if __name__ == "__main__":
+    unittest.main(failfast=True)
