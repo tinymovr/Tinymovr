1603f4169d405078b1aaaf69f42d439152ea3dfd
Ioannis Chatzikonstantinou <yconst@gmail.com>
2023-07-25 13:57:19 +0300

    add imports

--- studio/Python/tinymovr/config/__init__.py
+++ studio/Python/tinymovr/config/__init__.py
@@ -1,6 +1,8 @@
 from tinymovr.config.config import (
     get_bus_config,
     configure_logging,
+    tinymovr_definition,
+    bl_definition,
     create_device,
     create_device_with_hash_msg,
     ProtocolVersionError,
