a455f0ad53bd7105aef7c9041d19313582045636
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-11-10 13:44:02 +0200

    add observer init

--- firmware/src/observer/observer.c
+++ firmware/src/observer/observer.c
@@ -82,12 +82,79 @@ uint32_t observers_config_length(void)
     return config_length;
 }
 
+uint32_t observers_config_length(void)
+{
+    uint32_t config_length = sizeof(observer_commutation) + sizeof(observer_position);
+    for (uint8_t i=0; i<SENSOR_COUNT; i++)
+    {
+        if (observers[i].initialized)
+        {
+            config_length += sizeof(ObserverConfig);
+        }
+    }
+    return config_length;
+}
+
 bool observers_serialize_config_to_buffer(uint8_t *buffer, uint32_t *len)
 {
+    uint8_t *buffer_pos = buffer;
+    memcpy(buffer, &(observer_commutation->id), sizeof(observer_commutation->id));
+    buffer_pos += sizeof(observer_commutation->id);
+    memcpy(buffer, &(observer_position->id), sizeof(observer_position->id));
+    buffer_pos += sizeof(observer_position->id);
 
+    for (uint8_t i=0; i<SENSOR_COUNT; i++)
+    {
+        if (observers[i].initialized == true)
+        {
+            if (buffer_pos - buffer <= *len - sizeof(ObserverConfig))
+            {
+                memcpy(buffer_pos, &(observers[i].config), sizeof(ObserverConfig));
+                buffer_pos += sizeof(ObserverConfig);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    *len = buffer_pos - buffer;
+    return true;
 }
 
 bool observers_initialize_with_config_buffer(const uint8_t *buffer, const uint32_t len)
 {
+    uint8_t *buffer_pos = buffer;
+    uint32_t commutation_id, position_id;
+    memcpy(&commutation_id, buffer, sizeof(commutation_id));
+    buffer_pos += sizeof(commutation_id);
+    memcpy(&position_id, buffer, sizeof(position_id));
+    buffer_pos += sizeof(position_id);
 
+    ObserverConfig configs[SENSOR_COUNT] = {0};
+    for (uint8_t i=0; i<SENSOR_COUNT; i++)
+    {
+        if (buffer_pos - buffer >= *len - sizeof(ObserverConfig))
+        {
+            memcpy(&(configs[i]), buffer_pos, sizeof(ObserverConfig));
+            buffer_pos += sizeof(ObserverConfig);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    for (uint8_t i=0; i<SENSOR_COUNT; i++)
+    {
+        sensor_init_with_config(&(sensors[i]), &(configs[i]));
+        if (sensors[i].config.id == commutation_id)
+        {
+            sensor_commutation = &(sensors[i]);
+        }
+        else if (sensors[i].config.id == position_id)
+        {
+            sensor_position = &(sensors[i]);
+        }
+    }
+    return true;
 }
