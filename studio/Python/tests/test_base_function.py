2504b0b801b7716606f1aaa87f68c16ab9151ca8
Yannis Chatzikonstantinou <info@tinymovr.com>
2024-01-06 20:12:00 +0200

    change test limits

--- studio/Python/tests/test_base_function.py
+++ studio/Python/tests/test_base_function.py
@@ -22,15 +22,17 @@ class TestBaseFunction(TMTestCase):
         Test position control
         """
         self.check_state(0)
+        self.tm.motor.I_cal = 5
+        self.tm.controller.current.Iq_limit = 5
         self.try_calibrate()
         self.tm.controller.position_mode()
         self.check_state(2)
 
         for i in range(5):
-            self.tm.controller.position.setpoint = i * 10000 * ticks
+            self.tm.controller.position.setpoint = i * 3000 * ticks
             time.sleep(0.25)
             self.assertAlmostEqual(
-                i * 10000 * ticks, self.tm.encoder.position_estimate, delta=1000 * ticks
+                i * 3000 * ticks, self.tm.encoder.position_estimate, delta=1000 * ticks
             )
 
 
