c8f0a8f59880e7aa26d2d40b077e671a2588e57e
Yannis Chatzikonstantinou <info@tinymovr.com>
2024-03-26 19:00:46 +0200

    init all members

--- firmware/src/xfs.c
+++ firmware/src/xfs.c
@@ -4,5 +4,11 @@
 
 FramesConfig frames = {
     .position_sensor_to_user = DEFAULT_TRANSFORM,
-    .user_to_position_sensor = DEFAULT_TRANSFORM
+    .user_to_position_sensor = DEFAULT_TRANSFORM,
+    .position_sensor_to_motor = DEFAULT_TRANSFORM,
+    .motor_to_position_sensor = DEFAULT_TRANSFORM,
+    .commutation_sensor_to_motor = DEFAULT_TRANSFORM,
+    .motor_to_commutation_sensor = DEFAULT_TRANSFORM,
+    .motor_to_user = DEFAULT_TRANSFORM,
+    .user_to_motor = DEFAULT_TRANSFORM
 };
