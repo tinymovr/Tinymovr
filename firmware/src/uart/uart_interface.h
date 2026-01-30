400d130a33b2fef0116798fba846114dfa4a5f7a
Yannis Chatzikonstantinou <info@tinymovr.com>
2026-01-31 00:54:38 +0200

    fix constants

--- firmware/src/uart/uart_interface.h
+++ firmware/src/uart/uart_interface.h
@@ -24,8 +24,8 @@
 // UART Avlos Binary Protocol Constants
 #define UART_SYNC_BYTE          0xAA    // Frame start marker
 #define UART_FRAME_HEADER_SIZE  6       // Sync + Length + Hash + EP_ID(2) + CMD
-#define UART_FRAME_MIN_SIZE     9       // Header + CRC(2), no payload
-#define UART_FRAME_MAX_SIZE     17      // Header + 8 bytes payload + CRC(2)
+#define UART_FRAME_MIN_SIZE     8       // Header(6) + CRC(2), no payload
+#define UART_FRAME_MAX_SIZE     16      // Header(6) + 8 bytes payload + CRC(2)
 
 // Process received UART message (called from scheduler)
 void UART_process_message(void);
